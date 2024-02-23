ca729aad73a574f7774a86d60e5d50492674dd05
nemesis-9 <nyxn96@gmail.com>
2024-02-24 01:07:22 +0530

    Fixes for Auth

--- core/form/Form.php
+++ core/form/Form.php
@@ -3,12 +3,17 @@
 namespace app\core\form;
 
 use app\core\Model;
+use app\core\form\Field;
 
 class Form
 {
+    protected $html;
+
     public static function begin($action, $method)
     {
-        return sprintf('<form action="%s" method="%s">', $action, $method);
+        $form = new self();
+        $form->html = sprintf('<form action="%s" method="%s">', $action, $method);
+        return $form;
     }
 
     public static function end()
@@ -19,5 +24,10 @@ class Form
     public function field(Model $model, $attribute)
     {
         return new Field($model, $attribute);
+    }
+
+    public function __toString()
+    {
+        return $this->html . PHP_EOL;
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\core\Application;
+
 use app\controllers\SiteController;
+use app\controllers\AuthController;
 
 $app = new Application(dirname(__DIR__));
 
