d8015490191ec43e30fd20b0adfb41ad2f8bf39e
nemesis-9 <nyxn96@gmail.com>
2024-02-24 01:35:19 +0530

    phpdotenv package init

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit76ecdd2c1058425ba685604090fead37
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit76ecdd2c1058425ba685604090fead37', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit76ecdd2c1058425ba685604090fead37', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit76ecdd2c1058425ba685604090fead37
         call_user_func(\Composer\Autoload\ComposerStaticInit76ecdd2c1058425ba685604090fead37::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit76ecdd2c1058425ba685604090fead37::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
