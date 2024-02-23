438f092f57b241e1de38c0a93589497eb8e2cade
nemesis-9 <nyxn96@gmail.com>
2024-02-24 01:24:19 +0530

    Initializing migrations

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0001_initial
+{
+    public function up()
+    {
+        echo "Applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migrations";
+    }
+}
---
+++ migrations/m0002_newmig.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0002_newmig
+{
+    public function up()
+    {
+        echo "Applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migrations";
+    }
+}
